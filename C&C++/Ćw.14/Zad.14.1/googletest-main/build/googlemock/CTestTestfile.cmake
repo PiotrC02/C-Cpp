54df5baeb6c9bcd6cd5c0c9fd87b1a114c32543a
Piotr Cierczek <165421976+PiotrC02@users.noreply.github.com>
2024-03-30 18:08:18 +0100

    Add files via upload

---
+++ C&C++/Ćw.14/Zad.14.1/googletest-main/build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /cygdrive/c/Users/mrozmus/Desktop/Olka/Ćw. 14/googletest-main/googlemock
+# Build directory: /cygdrive/c/Users/mrozmus/Desktop/Olka/Ćw. 14/googletest-main/build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
